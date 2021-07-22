bbda36485961ff46f94e21a5f6e3a39191572a66
Robert Fuchs <robert.fuchs@logmein.com>
2021-07-22 22:00:20 +0200

    handle error when env variable is not set

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,29 @@ import countapi from "countapi-js";
 export const handler = async (event: APIGatewayEvent) => {
   console.log("Incoming event:", { event });
 
-  const result = await countapi.set(process.env.X_COUNT_API_KEY, 0);
-  if (result.status !== 200) {
-    console.error('Failed to reset counter');
+  const key = process.env.X_COUNT_API_KEY;
+  if (!key) {
+    const errorMessage =
+      'CountAPI key not defined as environment variable "X_COUNT_API_KEY"';
+    console.error(errorMessage);
     return {
       statusCode: 400,
+      body: errorMessage,
+    };
+  }
+
+  const result = await countapi.set(key, 0);
+  if (result.status !== 200) {
+    const errorMessage = "Failed to reset counter";
+    console.error(errorMessage);
+    return {
+      statusCode: 400,
+      body: errorMessage,
     };
   }
 
   return {
     statusCode: 200,
+    body: "Success!",
   };
 };
